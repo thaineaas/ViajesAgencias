749ab267e83d045773d57175fc6b09d5d097ba96
ediso9 <edison_gutierrez@usmp.pe>
2024-10-29 23:13:06 -0500

    Se añadio editar y eliminar de carrito y el modelo item carrito

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -13,6 +13,7 @@ public class ApplicationDbContext : IdentityDbContext
 
     public DbSet<Viajes.Models.Contacto> DataContacto {get; set; }
     public DbSet<Viajes.Models.Producto> DataProducto {get; set; }
+    public DbSet<Viajes.Models.ItemCarrito> DataItemCarrito {get; set; }
 
     public DbSet<Viajes.Models.Categoria> DataCategoria {get; set; }
 }
